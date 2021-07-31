f0b7492014ee737fdf2e56df576223c2549e99f5
FaisalAlshami <fasali2010@gmail.com>
2021-07-31 20:02:12 +0800

    Add files via upload

---
+++ bssfrontend/bssvue/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+
+<div>
+ <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+<!-- <Hello1  msg="welcome  to   myselft vue"  /> -->
+<router-view/>
+</div>
+
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue';
+// import Hello1 from './components/Hello1.vue';
+
+export default {
+
+  name: 'App',
+  // components: {
+  //   HelloWorld,
+  //   Hello1
+  // }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
